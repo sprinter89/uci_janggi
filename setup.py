7da30cad42cb7cb0f5355eef2a72925e5a3aaac7
sprinter89 <sprinter89tv@gmail.com>
2020-08-12 18:40:37 +0900

    idk

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows",
+        "Operating System :: Microsoft :: Windows",
     ],
     python_requires='>=3.8',
 )
